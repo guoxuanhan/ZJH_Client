fb1165e8427d76d116f28d0a4d6a5283978e4dc6
GuoJ <736071985@qq.com>
2022-03-08 22:42:22 +0800

    创建客户端unity工程

---
+++ MyClient/Assets/Scripts/Net/ClientPeer.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using UnityEngine;
+
+public class ClientPeer : MonoBehaviour
+{
+    private Socket clientSocket;
+
+    public ClientPeer()
+    {
+        try
+        {
+            clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        }
+        catch (System.Exception e)
+        {
+            Debug.LogError(e.Message);
+        }
+    }
+
+    /// <summary>
+    /// 连接服务器
+    /// </summary>
+    /// <param name="ip"></param>
+    /// <param name="port"></param>
+    public void Connect(string ip, int port)
+    {
+        try
+        {
+            clientSocket.Connect(new IPEndPoint(IPAddress.Parse(ip), port));
+            Debug.Log("连接服务器成功");
+            StartReceive();
+        }
+        catch (System.Exception e)
+        {
+            Debug.LogError(e.Message);
+        }
+    }
+
+    /// <summary>
+    /// 数据暂存区
+    /// </summary>
+    private byte[] receiveBuffer = new byte[1024];
+
+    /// <summary>
+    /// 开始接受数据
+    /// </summary>
+    private void StartReceive()
+    {
+        if (clientSocket == null && !clientSocket.Connected)
+        {
+            Debug.LogError("没有连接成功，无法接受消息");
+            return;
+        }
+
+        clientSocket.BeginReceive(receiveBuffer, 0, 1024, SocketFlags.None, ReceiveCallback, clientSocket);
+    }
+
+    /// <summary>
+    /// 开始接受数据完成后的回调
+    /// </summary>
+    /// <param name="ar"></param>
+    private void ReceiveCallback(IAsyncResult ar)
+    {
+        int length = clientSocket.EndReceive(ar);
+    }
+}
