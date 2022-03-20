f0b08692308fa54c5da1addb7ac5bd4c0a2bab6d
guoxuanhan <736081985@qq.com>
2022-03-20 20:15:31 +0800

    实现登录注册界面的UI跳转

---
+++ MyClient/Assets/Scripts/UI/LoginPanel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoginPanel : MonoBehaviour
+{
+    private InputField input_UserName;
+    private InputField input_Password;
+    private Button btn_Login;
+    private Button btn_Register;
+
+    private void Awake()
+    {
+        EventCenter.AddListener(EventDefine.ShowLoginPanel, Show);
+        Init();
+    }
+
+    private void Init()
+    {
+        input_UserName = transform.Find("input_UserName").GetComponent<InputField>();
+        input_Password = transform.Find("input_Password").GetComponent<InputField>();
+        btn_Login = transform.Find("btn_Login").GetComponent<Button>();
+        btn_Register = transform.Find("btn_Register").GetComponent<Button>();
+        btn_Register.onClick.AddListener(OnRegisterClick);
+        gameObject.SetActive(true);
+    }
+    
+    private void OnDestroy()
+    {
+        EventCenter.RemoveListener(EventDefine.ShowLoginPanel, Show);
+    }
+
+    /// <summary>
+    /// 注册按钮点击
+    /// </summary>
+    private void OnRegisterClick()
+    {
+        EventCenter.Broadcast(EventDefine.ShowRegisterPanel);
+    }
+
+    private void Show()
+    {
+        gameObject.SetActive(true);
+    }
+}
---
+++ MyClient/Assets/Scripts/UI/RegisterPanel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RegisterPanel : MonoBehaviour 
+{
+	private InputField input_UserName;
+	private InputField input_Password;
+	private Button btn_Back;
+	private Button btn_Register;
+	private Button btn_Pwd;
+
+	private void Awake()
+	{
+		EventCenter.AddListener(EventDefine.ShowRegisterPanel, Show);
+		Init();
+	}
+
+	private void Init()
+	{
+		input_UserName = transform.Find("UserName/input_UserName").GetComponent<InputField>();
+		input_Password = transform.Find("Password/input_Password").GetComponent<InputField>();
+		btn_Back = transform.Find("btn_Back").GetComponent<Button>();
+		btn_Register = transform.Find("btn_Register").GetComponent<Button>();
+		btn_Pwd = transform.Find("btn_Pwd").GetComponent<Button>();
+		btn_Back.onClick.AddListener(OnBackClick);
+		gameObject.SetActive(false);
+	}
+	
+	private void OnDestroy()
+	{
+		EventCenter.RemoveListener(EventDefine.ShowRegisterPanel, Show);
+	}
+
+	/// <summary>
+	/// 返回点击按钮
+	/// </summary>
+	private void OnBackClick()
+	{
+		gameObject.SetActive(false);
+		EventCenter.Broadcast(EventDefine.ShowLoginPanel);
+	}
+	
+	private void Show()
+	{
+		gameObject.SetActive(true);
+	}
+}
